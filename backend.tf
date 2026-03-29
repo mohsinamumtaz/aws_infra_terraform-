03efd22e8b8d2ba645329f722d71afc54fd3fa89
avinash <you@example.com>
2026-03-29 21:10:24 +0530

    2nd

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "project/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
